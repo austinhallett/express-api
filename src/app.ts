a7733d2d7f4478a51ef7e804eef91b78f16a4f8d
auhallet <austin.hallett-doughty@silabs.com>
2022-02-25 13:33:50 -0500

    log requests

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
-import express from "express";
+import express, {
+    Express, 
+    Request,
+    Response, 
+    NextFunction} from "express";
 import config from 'config';
 import connect from './utils/connect';
 import logger from './utils/logger';
+import routes from "./routes";
 
 
 const port = config.get<number>('port');
@@ -10,4 +15,10 @@ const app = express();
 app.listen(port, async () => {
     logger.info(`Server running on port http://localhost:${port}`);
     await connect();
+    routes(app);
 })
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+    logger.info(`[${req.method}] ${req.url} ${res.statusCode}`);
+    next();
+});
