248de609f86efb49228cb06751ad1ca986500af3
auhallet <austin.hallett-doughty@silabs.com>
2022-02-28 12:39:30 -0500

    created Add example route [POST]

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,12 @@
 import {Express, Request, Response} from 'express';
+import { createExampleHandler } from './controller/example.controller';
+import validateResource from './middleware/validateResource';
+import { createExampleSchema } from './schema/example.schema';
 
 function routes(app: Express){
     app.get("/health", (req: Request, res: Response) => res.sendStatus(200));
+
+    app.post("/api/example", validateResource(createExampleSchema), createExampleHandler);
 };
 
 export default routes;
