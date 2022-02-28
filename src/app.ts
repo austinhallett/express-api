1b1a9634a9508da89b2f0864dbed9ba45641e128
auhallet <austin.hallett-doughty@silabs.com>
2022-02-28 12:41:37 -0500

    express.json middleware

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,8 @@ import routes from "./routes";
 const port = config.get<number>('port');
 const app = express();
 
+app.use(express.json())
+
 app.listen(port, async () => {
     logger.info(`Server running on port http://localhost:${port}`);
     await connect();
